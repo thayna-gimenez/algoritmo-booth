ba9526197820706b36ca8dfeaaa5553e1c09a5b8
thayná <88508228+thayna-gimenez@users.noreply.github.com>
2023-09-04 12:11:34 -0400

    código terminado

--- algoritmo-booth.c
+++ algoritmo-booth.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
-void ler_entrada() {
-    
+void printar(int matriz[2][32], int len_negativo1, FILE *arquivo_saida) {
+    int i, j;
+    for (i = 0; i < 2; i++) {
+        for (j = len_negativo1 - 1; j >= 0; j--) {
+            fprintf(arquivo_saida, "%d", matriz[i][j]);
+        }
+        
+        if (i == 0) {
+            fprintf(arquivo_saida, " ");
+        }
+    }
 }
 
 int decimal_binario(int vetor[], int num){
@@ -77,7 +87,69 @@ int decimal_binario(int vetor[], int num){
     }
 }
 
-void deslocamento_direita(int matriz[2][32], int *QP, int len_negativo1) {
+long binario_decimal(int matriz[2][32], int len_negativo1) {
+    int i, j, multiplicador = 1;
+    long valor, resultado = 0;
+    
+    if (matriz[0][len_negativo1 - 1] == 1) {
+        multiplicador = -1;
+        // fazendo o complemento
+        for (i = 0; i < 2; i++) {
+            for (j = 31; j >= 0; j--) {
+                if (matriz[i][j] == 0) {
+                    matriz[i][j] = 1;
+                }
+                else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+        
+        // fazendo a soma
+        int carry = 1;
+        int valor_soma;
+        
+        for (i = 1; i >= 0; i--) {
+            for(j = 0; j < 32; j++){
+                // se o carry for zero, não precisa fazer a soma porque tudo vai ficar igual independente
+                if (carry == 0) {
+                    break;
+                }
+                
+                valor_soma = matriz[i][j] + carry;
+    
+                // se o valor da soma for 2, o carry continua 1 e o resultado fica 0
+                if (valor_soma > 1) {
+                    carry = 1;
+                    matriz[i][j] = 0;
+                }
+                // senão, o resultado fica um e o carry zera, e o resto do número continua igual
+                else {
+                    carry = 0;
+                    matriz[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    for (i = 1; i >= 0; i--) {
+        for (j = 0; j < len_negativo1; j++) {
+            if (i == 0) {
+                valor = pow(2, len_negativo1);
+            }
+            else {
+                valor = 1;
+            }
+            
+            resultado += matriz[i][j] * valor * pow(2, j);
+        }
+    }
+    
+    // se o binário for 1, multiplica por -1 (o número é negativo)
+    return resultado * multiplicador;
+}
+
+void deslocamento_direita(int matriz[2][32], int *QP, int len_negativo1, FILE *arquivo_saida) {
     int i, j;
     
     // o binário está ao contrário, então percorre a matriz da esquerda para a deslocamento_direita
@@ -99,15 +171,12 @@ void deslocamento_direita(int matriz[2][32], int *QP, int len_negativo1) {
     }
     
     // printando o resultado
-    for (i = 0; i < 2; i++) {
-        for (j = len_negativo1 - 1; j>= 0; j--) {
-            printf("%d", matriz[i][j]);
-        }
-    }
-    printf("\n");
+    fprintf(arquivo_saida, "Deslocamento a direita: ");
+    printar(matriz, len_negativo1, arquivo_saida);
+    fprintf(arquivo_saida, "\n");
 }
 
-void soma(int matriz1[2][32], int matriz2[2][32], int len_negativo1) {
+void soma(int matriz1[2][32], int matriz2[2][32], int len_negativo1, FILE *arquivo_saida) {
     // matriz1 -> P e matriz2 -> S ou A
     int i, j, carry = 0, valor_soma;
     
@@ -125,140 +194,140 @@ void soma(int matriz1[2][32], int matriz2[2][32], int len_negativo1) {
         }
     }
     
-    for (i = 0; i < 2; i++) {
-        for (j = len_negativo1 - 1; j >= 0; j--) {
-            printf("%d", matriz1[i][j]);
-        }
-    }
-    
-    printf("\n");
+    // printando o resultado
+    printar(matriz1, len_negativo1, arquivo_saida);
+    fprintf(arquivo_saida, "\n");
 }
 
 int main(){
     int n1, binario_um[32], binario_negativo1[32], len_binario1, len_negativo1;
     int n2, binario_dois[32], len_binario2;
     int i, j;
+    FILE *arquivo = fopen("teste.txt", "r");
+    FILE *arquivo_saida = fopen("saida.txt", "wt");
     
-    scanf("%d %d", &n1, &n2);
-
-    // chamando as funções de transformação em binário, que retorna a quantidade de bits de cada um
-    len_binario1 = decimal_binario(binario_um, n1); // binário do primeiro número e qtde de bits dele
-    len_negativo1 = decimal_binario(binario_negativo1, -n1); // binário do primeiro número negativo e qtde de bits dele
-    len_binario2 = decimal_binario(binario_dois, n2); // binário do segundo número e qtde de bits dele
-    
-    // igualando a quantidade de bits entre os números
-    // se um dos binários tiver 4 bits e o outro tiver 5, usa a quantidade de bits do maior (5)
-    if (len_negativo1> len_binario2) {
-        len_binario2 = len_negativo1;
-        len_binario1 = len_negativo1;
+    if (arquivo == NULL) {
+        printf("Arquivo de teste não encontrado!");
+        fclose(arquivo_saida);
+        fclose(arquivo);
+        return -1;
     }
     else {
-        len_negativo1 = len_binario2;
-        len_binario1 = len_binario2;
-    }
-    
-    // printando os binários com sinal positivo ou negativo, apenas com a quantidade de bits estabelecida, não 32
-    printf("(%+d): ", n1);
-    for (i = len_binario1 - 1; i >= 0; i--) {
-        printf("%d", binario_um[i]);
-    }
-    printf("\n");
-    
-    printf("(%+d): ", n2);
-    for (i = len_binario2 - 1; i >= 0; i--) {
-        printf("%d", binario_dois[i]);
-    }
-    printf("\n");
-    
-    printf("(%+d): ", -n1);
-    for (i = len_negativo1 - 1; i >= 0; i--){
-        printf("%d", binario_negativo1[i]);
-    }
-    printf("\n\n");
-    
-    // criando as matrizes com os binários
-    // duas linhas e cada linha tem 32 colunas, mas pode ser len_negativo1 (pra não ter que rodar por 32 bits) 
-    // QA, QS e QP é pro último bit separado do resto 
-    int A[2][32], QA = 0;
-    int S[2][32], QS = 0;
-    int P[2][32], QP = 0;
-    
-    for (i = 0; i < 32; i++) {
-        A[0][i] = binario_um[i];
-        A[1][i] = 0;
-    }
-    
-    for (i = 0; i < 32; i++) {
-        S[0][i] = binario_negativo1[i];
-        S[1][i] = 0;
-    }
-    
-    for (i = 0; i < 32; i++) {
-        P[0][i] = 0;
-        P[1][i] = binario_dois[i];
-    }
-
-    // printando A
-    printf("A: ");
-    for (i = 0; i < 2; i++) {
-        // len_negativo1 pra nao printar 32 bits
-        for (j = len_negativo1 - 1; j >= 0; j--) {
-            printf("%d", A[i][j]);
-        }
-        printf(" ");
-    }
-    
-    printf("%d\n", QA);
-
-    // printando S
-    printf("S: ");
-    for (i = 0; i < 2; i++) {
-        for (j = len_negativo1 - 1; j >= 0; j--) {
-            printf("%d", S[i][j]);
-        }
-        printf(" ");
-    }
-    
-    printf("%d\n", QS);
-
-    // printando P
-    printf("P: ");
-    for (i = 0; i < 2; i++) {
-        for (j = len_negativo1 - 1; j >= 0; j--) {
-            printf("%d", P[i][j]);
-        }
-        printf(" ");
-    }
-    
-    printf("%d\n\n", QP);
-    
-    // printando o processo de soma e deslocamento
-    for (i = 0; i < len_negativo1; i++) {
-        if (P[1][0] == QP) {
-            deslocamento_direita(P, &QP, len_negativo1);
-        }
-        else if (P[1][0] == 1 && QP == 0) {
-            soma(P, S, len_negativo1);
-            deslocamento_direita(P, &QP, len_negativo1);
+        if(arquivo_saida == NULL) {
+            printf("Arquivo de saída não criado!");
+            fclose(arquivo_saida);
+            fclose(arquivo);
+            return -1;
         }
         else {
-            soma(P, A, len_negativo1);
-            deslocamento_direita(P, &QP, len_negativo1);
+            while (fscanf(arquivo, "%d %d", &n1, &n2) == 2) {
+                
+                // chamando as funções de transformação em binário, que retorna a quantidade de bits de cada um
+                len_binario1 = decimal_binario(binario_um, n1); // binário do primeiro número e qtde de bits dele
+                len_negativo1 = decimal_binario(binario_negativo1, -n1); // binário do primeiro número negativo e qtde de bits dele
+                len_binario2 = decimal_binario(binario_dois, n2); // binário do segundo número e qtde de bits dele
+                
+                // igualando a quantidade de bits entre os números
+                // se um dos binários tiver 4 bits e o outro tiver 5, usa a quantidade de bits do maior (5)
+                if (len_negativo1> len_binario2) {
+                    len_binario2 = len_negativo1;
+                    len_binario1 = len_negativo1;
+                }
+                else {
+                    len_negativo1 = len_binario2;
+                    len_binario1 = len_binario2;
+                }
+                
+                // printando os binários com sinal positivo ou negativo, apenas com a quantidade de bits estabelecida, não 32
+		fprintf(arquivo_saida, "Números informados: %d e %d\n\n", n1, n2);
+
+                fprintf(arquivo_saida, "(%+d): ", n1);
+                for (i = len_binario1 - 1; i >= 0; i--) {
+                    fprintf(arquivo_saida, "%d", binario_um[i]);
+                }
+                fprintf(arquivo_saida, "\n");
+                
+                fprintf(arquivo_saida, "(%+d): ", n2);
+                for (i = len_binario2 - 1; i >= 0; i--) {
+                    fprintf(arquivo_saida, "%d", binario_dois[i]);
+                }
+                fprintf(arquivo_saida, "\n");
+                
+                fprintf(arquivo_saida, "(%+d): ", -n1);
+                for (i = len_negativo1 - 1; i >= 0; i--){
+                    fprintf(arquivo_saida, "%d", binario_negativo1[i]);
+                }
+                fprintf(arquivo_saida, "\n\n");
+                
+                // criando as matrizes com os binários
+                // duas linhas e cada linha tem 32 colunas, mas pode ser len_negativo1 (pra não ter que rodar por 32 bits) 
+                // QA, QS e QP é pro último bit separado do resto 
+                int A[2][32], QA = 0;
+                int S[2][32], QS = 0;
+                int P[2][32], QP = 0;
+                
+                for (i = 0; i < 32; i++) {
+                    A[0][i] = binario_um[i];
+                    A[1][i] = 0;
+                }
+                
+                for (i = 0; i < 32; i++) {
+                    S[0][i] = binario_negativo1[i];
+                    S[1][i] = 0;
+                }
+                
+                for (i = 0; i < 32; i++) {
+                    P[0][i] = 0;
+                    P[1][i] = binario_dois[i];
+                }
+            
+                // printando A
+                fprintf(arquivo_saida, "A: ");
+                printar(A, len_negativo1, arquivo_saida);
+                fprintf(arquivo_saida, "%d\n", QA);
+            
+                // printando S
+                fprintf(arquivo_saida, "S: ");
+                printar(S, len_negativo1, arquivo_saida);
+                fprintf(arquivo_saida, "%d\n", QS);
+            
+                // printando P
+                fprintf(arquivo_saida, "P: ");
+                printar(P, len_negativo1, arquivo_saida);
+                
+                fprintf(arquivo_saida, "%d\n\n", QP);
+                
+                // printando o processo de soma e deslocamento
+                for (i = 0; i < len_negativo1; i++) {
+                    if (P[1][0] == QP) {
+                        deslocamento_direita(P, &QP, len_negativo1, arquivo_saida);
+                    }
+                    else if (P[1][0] == 1 && QP == 0) {
+                        fprintf(arquivo_saida, "Soma de P e S: ");
+                        soma(P, S, len_negativo1, arquivo_saida);
+                        deslocamento_direita(P, &QP, len_negativo1, arquivo_saida);
+                    }
+                    else {
+                        fprintf(arquivo_saida, "Soma de P e A: ");
+                        soma(P, A, len_negativo1, arquivo_saida);
+                        deslocamento_direita(P, &QP, len_negativo1, arquivo_saida);
+                    }
+                }
+                
+                fprintf(arquivo_saida, "\n");
+                
+                // printando resultado final
+                fprintf(arquivo_saida, "Resultado de %d * %d: ", n1, n2);
+                printar(P, len_negativo1, arquivo_saida);
+                fprintf(arquivo_saida, ", que é %ld", binario_decimal(P, len_negativo1));
+                fprintf(arquivo_saida, "\n\n\n");
+            }
         }
+    
     }
     
-    printf("\n");
-    
-    // printando resultado final
-    for(i = 0; i < 2; i++) {
-        for (j = len_negativo1 - 1; j >= 0; j--) {
-            printf("%d", P[i][j]);
-        }
-        
-        printf(" ");
-    }
-    
-    printf("%d", QP);
+    fclose(arquivo_saida);
+    fclose(arquivo);
     
     return 0;
     
